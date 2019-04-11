1d9aa63214dcabc4bfc8657bab3311cd80be0416
lunarscents <45254522+lunarscents@users.noreply.github.com>
2019-04-11 22:21:56 +0900

    [hackerrank] - 30 Days of Code > Day2: Data Types

---
+++ HackeRank/30Days/Day 1 : Data Types.js
@@ -0,0 +1,37 @@
+/** First Creation Code before 2 years ago **/
+// Declare second integer, double, and String variables.
+var first = +input_stdin_array[0];
+var second = +input_stdin_array[1];
+var third = input_stdin_array[2];
+
+// Read and save an integer, double, and String to your variables.
+
+// Print the sum of both integer variables on a new line.
+console.log(i + first);
+
+// Print the sum of the double variables on a new line.
+console.log((d + second).toFixed(1));
+
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s+third);
+    
+/** Second Creation Code **/
+// Declare second integer, double, and String variables.
+let first, second, third;
+
+// Read and save an integer, double, and String to your variables.
+first = parseInt(readLine());
+second = parseFloat(readLine());
+third = readLine();
+
+// Print the sum of both integer variables on a new line.
+console.log(i + first);
+
+// Print the sum of the double variables on a new line.
+console.log((d + second).toFixed(1));
+
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+console.log(s + third);
